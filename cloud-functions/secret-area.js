c2f8a19258db2425812fc8b0c4ba3b40cd2a6026
bindzo <hpesc1133@gmail.com>
2020-09-02 02:44:59 +0700

    secret

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,20 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
-}
+exports.handler = function (event, context, callback) {
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: "<h1>Success</h1>",
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
+};
